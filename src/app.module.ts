877faf0ea94b219cd467f429ddedc9cd47e9ee12
ivangricenko <0.ivangricenko.0@gmail.com>
2021-06-07 23:47:38 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  controllers: [],
-  providers: [],
+  imports: [TrackModule],
 })
 export class AppModule {}
